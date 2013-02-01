51d100677ab1b9ddf38ad1d5b06d0a72e5415feb
joseespinalg <jose@familiaespinal.vv.si>
2013-02-01 14:58:42 -0400

    Update facebook.py

--- facebook.py
+++ facebook.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python 
+
+
+import gtk
+import webkit
+import gobject
+
+
+
+gobject.threads_init()
+win = gtk.Window()
+win.set_size_request(1024, 800)
+win.set_title("Facebook for Ubuntu")
+win.set_resizable(False)
+win.set_position(gtk.WIN_POS_CENTER)
+bro = webkit.WebView()
+bro.open("http://m.facebook.com")
+win.add(bro)
+win.show_all()
+
+gtk.main()
